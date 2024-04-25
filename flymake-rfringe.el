22c9f992e64313590ee2b2338ca038559220aa3f
adelplanque <alaindelplanque@mailoo.org>
2024-04-25 08:40:51 +0200

    doc

--- flymake-rfringe.el
+++ flymake-rfringe.el
@@ -1,95 +1,59 @@
-;;; flymake-rfringe.el --- display the relative location of the region, in the fringe.
-;;
-;; Author     : Dino Chiesa <dpchiesa@hotmail.com>
-;; Created    : April 2011
-;; Version    : 1.0
-;; Keywords   : fringe, bitmap
-;; X-URL      : http://www.emacswiki.org/emacs/rfringe.el
-;; Last-saved : <2011-April-05 11:20:26>
+;;; flymake-rfringe.el --- Report flymake diagnostics as relative right fringe.
 
 ;; Copyright (C) 2011 Dino Chiesa
-;;
-;; This file is NOT part of GNU Emacs and is licensed differently.
-;; flymake-rfringe.el is licensed under the Ms-PL.  See the full copy of that
-;; license for more details. http://www.opensource.org/licenses/ms-pl
-;;
+
+;; Author: Dino Chiesa <dpchiesa@hotmail.com>
+;;         Alain Delplanque <alaindelplanque@mailoo.org>
+;; Maintainer: Alain Delplanque <alaindelplanque@mailoo.org>
+;; Version: 1.0
+;; Keywords: flymake, fringe
+;; URL: https://github.com/adelplanque/flymake-rfringe
+
+;; This file is NOT part of GNU Emacs.
+
+;;; Licence:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; Original rfringe.el code is licensed differently, under the Ms-PL.  For more
+;; details, see: http://www.opensource.org/licenses/ms-pl
 
 ;;; Commentary:
-;;
-;; This is a module to allow the use of the fringe to indicate locations
-;; relative to the bounds of the buffer.  flymake-rfringe = "relative fringe".
-;;
-;; In emacs, displaying fringe indicators is done via text overlays. In
-;; that way, bitmaps in the fringe are attached to the lines of text
-;; shown in the buffer window.
-;;
-;; This works nicely when the fringe is used to indicate information
-;; that is relevant to the adjacent line; for example, text overflow, or
-;; something similar. But, there isn't a simple way for an application
-;; or module to use the fringe to display buffer-relative information -
-;; for example, the location of compiler error messages.
-;;
-;; In fact, using the fringe to communicate this kind of information -
-;; buffer-relative positions - is probably more intuitive and is
-;; certainly more useful for the user. For example, consider the
-;; scrollbar. The position and size of the scrollbar indicates to the
-;; user the position and size of the current window relative to the
-;; entire buffer. This is information that cound not be easily or
-;; appropriately conveyed within the visual text. The fringe is
-;; perfectly suited for this purpose.
-;;
-;; Along those lines, a useful integration of fringe with flymake would
-;; be to use fringe bitmaps visually indicate the position of all
-;; flymake errors and warnings in the buffer, relative to the beginning
-;; and end of the buffer. A quick glance at the fringe would give a
-;; visual indication of the number of errors or warnings and their
-;; approximate positions.
-;;
-;; Likewise, a diff mode might want to display fringe indicators for the
-;; number and approximate relative position of differences.
-;;
-;; Doing this is not simple, because of the dependency of fringe bitmaps
-;; on text overlays that I described above.  To use the fringe to
-;; communicate information regarding buffer-relative positions requires
-;; a transformation from "buffer position" to "window position".  And
-;; this transformation must be re-computed each time a window scrolls or
-;; changes size.
-;;
-;; This module addresses that need, and provides that transformation. It
-;; allows you to set an indicator that is buffer-relative in the fringe;
-;; the indicator automatically redisplays if the window changes size, or
-;; scrolls.
-;;
-;; Examples:
-;;
-;; 1. In the simplest case, you can use rfringe to provide a visual
-;;    indicator of the top of the region in the buffer, like so:
-;;
-;;       (flymake-rfringe-show-region)
-;;
-;;    To turn off the indicator, do this:
-;;
-;;       (flymake-rfringe-hide-region)
-;;
-;;
-;; 2. You can also use rfringe to display a set of indicators,
-;;    corresponding to a set of locations in the buffer. These might be
-;;    the locations of compiler errors, or section beginnings, or
-;;    whatever you like.
-;;
-;;       (setq posns '(79 1000 2000 3000 4000 5000 6000 9000 10000))
-;;       (mapc 'flymake-rfringe-create-relative-indicator posns)
-;;
-;;    As you scroll through the buffer, the indicators in the fringe remain fixed.
-;;
-;;    To remove the indicators, do this:
-;;
-;;       (flymake-rfringe-remove-managed-indicators)
-;;
-;;    By default, flymake-rfringe defines advice to extend flymake to display
-;;    indicators this way.  This is not the only intended use of flymake-rfringe.el,
-;;    but it is a good example.
-;;
+
+;; flymake-rfringe is a small module allowing the display of flymake diagnostics
+;; in the form of a small mark in the right fringe.  The marking take place in
+;; the visible window in proportion to the diagnostic line within the entire
+;; buffer.
+
+;; The style of the marks can be customized with the faces:
+;; * `flymake-rfringe-error-face'
+;; * `flymake-rfringe-warning-face'
+;; * `flymake-rfringe-note-face'
+
+;; flymake-rfringe works by advising functions since flymake does not seem to
+;; offer a hook allowing you to be notified of a diagnostic change.
+
+;; Usage:
+
+;; (require 'flymake-rfringe)
+
+;; History:
+
+;; flymake-rfringe started as a fork of Dino Chiesa's rfringe.el to make the
+;; module compatible with the version of flymake shipped as standard in Emacs
+;; before evolving into a deeper rewrite.
+
 ;;; Code:
 
 (require 'compile)
@@ -102,27 +66,30 @@
 
 (defface flymake-rfringe-error-face
   '((t :inherit compilation-error))
-  "Face for error mark.")
+  "Face for error mark."
+  :group 'flymake-rfringe)
 
 (defface flymake-rfringe-warning-face
   '((t :inherit compilation-warning))
-  "Face for warning mark.")
+  "Face for warning mark."
+  :group 'flymake-rfringe)
 
 (defface flymake-rfringe-note-face
   '((t :inherit compilation-info))
-  "Face for comment mark.")
+  "Face for comment mark."
+  :group 'flymake-rfringe)
 
 (defconst flymake-rfringe-type-rank-alist
   '((:error . 2)
     (:warning . 1)
     (:note . 0))
-  "A alist which associate a rank to mark type.")
+  "A alist which associate a rank to flymake diagnostic type.")
 
 (defconst flymake-rfringe-type-face-alist
   '((:error . flymake-rfringe-error-face)
     (:warning . flymake-rfringe-warning-face)
     (:note . flymake-rfringe-note-face))
-  "A alist which associate a face to mark type.")
+  "A alist which associate a face to flymake diagnostic type.")
 
 ;; flymake-rfringe displays only one kind of bitmap - a thin dash. Create it here.
 (define-fringe-bitmap 'flymake-rfringe-thin-dash [255 0])
@@ -188,7 +155,7 @@ arguments are R."
   (mapc (lambda (item) (flymake-rfringe--create-indicator item))
         (flymake-diagnostics)))
 
-;; hooks for managing all managed indicators
+;; hooks for managing all indicators
 (add-hook 'window-scroll-functions #'flymake-rfringe--update-indicators-on-window-scroll)
 (add-hook 'window-configuration-change-hook #'flymake-rfringe--reset-indicators)
 (advice-add 'flymake--handle-report :after #'flymake-rfringe--post-syntax-check)
